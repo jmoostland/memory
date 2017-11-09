6f58c2d378d4e15f94fce507d069f8cf1dafeccd
jmoostland <jeanineoostland@gmail.com>
2017-11-09 14:01:25 +0100

    second commit

--- index.php
+++ index.php
@@ -1,17 +1,58 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+
+        <script>
+            function changePhoto(d) {
+                
+                document.getElementById('d').src = '/images/tiger.jpg';
+            }
+
+        </script>
+
+        <style>
+            img{
+                width: 100px;
+                height: 100px;
+                vertical-align: bottom;
+
+            }
+            td{
+                padding: 0px;
+            }
+
+            div{
+                margin: 200px auto;
+                width: 50%;
+                padding: 0px;
+            }
+        </style>
+
     </head>
     <body>
+        <div>
+            <table border="1">
+                <?php
+                for ($row = 0; $row <= 4; $row++) {
+                    echo "<tr>";
+                    for ($kolom = 0; $kolom <= 4; $kolom++) {
+                        $id="id".$row.$kolom;
+                        echo "<td><img id=$id src=images/memory.jpg onclick=changePhoto($id)></td>";
+                    }
+                    echo "</tr>";
+                }
+                ?>
+            </table>
+
+
+        </div>                
+
+
         <?php
-        // put your code here
+// put your code here
         ?>
     </body>
 </html>
